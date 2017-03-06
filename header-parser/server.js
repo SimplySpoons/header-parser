dacb3e107f6a02377c41d26fda2c08e25fd51a32
Luke Rossi <luke.rossi@dealer.com>
2017-03-06 18:45:10 +0000

    server file commit

--- header-parser/server.js
+++ header-parser/server.js
@@ -1,1 +1,13 @@
-var express = require('express')
+'use strict'; // enable 'strict' mode
+
+var app = server();
+var server = require('express')
+var routes = require('./app/routes/index.js');
+
+var port = process.env.PORT || 8080; //define port
+
+routes(app);
+
+app.listen(port, function(){
+  console.log("App is listening on port: " + port); //log the port in console
+});
