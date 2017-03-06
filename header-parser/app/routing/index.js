8bd5a95226ce7a74f5c72f5c85c7e9ce9800ddb4
Luke Rossi <luke.rossi@dealer.com>
2017-03-06 19:13:41 +0000

    updating index.js

--- header-parser/app/routing/index.js
+++ header-parser/app/routing/index.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         
         var ipaddr = req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||
         req.connection.remoteAddress || 
-        req.connection.socket.remoteAddress ||
-        req.socket.remoteAddress;
+        req.connection.socket.remoteAddress;
         
         var userinfo = {
          'ip-address': req.ipaddr, // request IP Address
