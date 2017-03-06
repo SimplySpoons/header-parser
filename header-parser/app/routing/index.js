4740289e4d7d5c68eabdd351ded522b899fe2863
Luke Rossi <luke.rossi@dealer.com>
2017-03-06 19:13:02 +0000

    updating index.js

--- header-parser/app/routing/index.js
+++ header-parser/app/routing/index.js
@@ -6,16 +6,16 @@ module.exports = function(app) {
         
         var ipaddr = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress || 
-        req.connection.socket.remoteAddress;
-        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress ||
+        req.socket.remoteAddress;
         
         var userinfo = {
          'ip-address': req.ipaddr, // request IP Address
          'language': req.headers["accept-language"].split(",")[0], // request user language
          'software': req.headers['user-agent'].split("(")[1].split(")")[0] // request user OS
         };
-        
+
      res.send(userinfo); // send user info
-     
+
     });
 }; 
