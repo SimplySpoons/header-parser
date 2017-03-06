318e98f7a6ada26b2d08a3fef9dbddb262617446
Luke Rossi <luke.rossi@dealer.com>
2017-03-06 20:00:56 +0000

    -m changed server and index files for testing purposes

--- header-parser/app/routing/index.js
+++ header-parser/app/routing/index.js
@@ -1,18 +1,46 @@
-'use strict'; // enable 'strict' mode
+// 'use strict'; // enable 'strict' mode
 
-var app = require('express')(); // call express
+// var app = require('express')(); // call express
 
-app.get('/api/whoami', (req, res) => {
+// app.get('/api/whoami', (req, res) => {
 
-  var userinfo = {
-    "ipaddress": req.ip, // request user IP Address
-    "language": req.headers["accept-language"].split(",")[0], // request user language
-    "software": req.headers["user-agent"].split("(")[1].split(")")[0] // request user OS
-  };
+//   var userinfo = {
+//     "ipaddress": req.ip, // request user IP Address
+//     "language": req.headers["accept-language"].split(",")[0], // request user language
+//     "software": req.headers["user-agent"].split("(")[1].split(")")[0] // request user OS
+//   };
 
-  console.log(JSON.stringify(userinfo) + "\n");
+//   console.log(JSON.stringify(userinfo) + "\n");
 
-  res.set('Content-Type', 'application/json');
-  res.json(userinfo);
+//   res.set('Content-Type', 'application/json');
+//   res.json(userinfo);
   
+// });
+
+var server = require('express');
+var app = server();
+var useragent = require('useragent');
+
+var port = process.env.PORT || 3500;
+
+app.listen(port, function(){
+  console.log("App is listening on port: " + port);
+});
+
+app.get('/', function(req, res) {
+  var agent = useragent.parse(req.headers['user-agent']);
+
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
+
+  res.json({
+    ip: ipAddr,
+    "language": req.headers['accept-language'].split(',')[0],
+    OS: agent.os.family
+  });
 });
--- header-parser/server.js
+++ header-parser/server.js
@@ -1,11 +1,11 @@
-'use strict'; // enable 'strict' mode
+// 'use strict'; // enable 'strict' mode
 
-var server = require('express');
-var app = server();
-var routes = require('./app/routing/index.js');
+// var server = require('express');
+// var app = server();
+// var routes = require('./app/routing/index.js');
 
-var port = process.env.PORT || 8080; //define port
+// var port = process.env.PORT || 8080; //define port
 
-app.listen(port, function(){
-  console.log("App is listening on port: " + port); //log the port in console
-});
+// app.listen(port, function(){
+//   console.log("App is listening on port: " + port); //log the port in console
+// });
