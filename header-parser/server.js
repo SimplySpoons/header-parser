cc1ec282db3e0e2eb80661be09edaac633409506
Luke Rossi <luke.rossi@dealer.com>
2017-03-07 17:42:29 +0000

    changes to server.js

--- header-parser/server.js
+++ header-parser/server.js
@@ -1,17 +1,31 @@
-'use strict';
+"use strict";
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var routes = require('./app/routing/index.js');
-    
-var port = process.env.PORT || 8080;        // set our port
-    
-// The format follows as, alias to use for real path, also allows permission to such path.
-//app.use('/api', express.static(process.cwd() + '/app/api'));
-    
-routes(app);
+// let favicon = require('serve-favicon');
+let app = require('express')();
 
-app.listen(port, function() {
-    console.log('Node.js listening on port ' + port);
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// app.get("/", (req, res) => {
+//   // return instructions page
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+app.get('/api/whoami', (req, res) => {
+
+  let info = {
+    "ipaddress": req.ip,
+    "language": req.headers["accept-language"].split(",")[0],
+    "software": req.headers["user-agent"].split("(")[1].split(")")[0]
+  };
+
+  console.log(JSON.stringify(info) + "\n");
+
+  res.set('Content-Type', 'application/json');
+  res.json(info);
+});
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port + "\n");
 });
