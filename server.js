d1557c04745f9cf217c140eff386567ce061684a
Luke Rossi <luke.rossi@dealer.com>
2017-03-07 18:07:33 +0000

    fixed server.js function call placement

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict'; // 'strict' mode
 
-var app = express();
 var routes = require('./app/routing/index.js');
-var express = require('express');        
+var express = require('express'); 
+var app = express();
     
 var port = process.env.PORT || 8080;      
 
