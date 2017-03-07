811f1f6a56224883f10b8769aee45e7eca8eeac0
Luke Rossi <luke.rossi@dealer.com>
2017-03-07 17:47:11 +0000

    test fixes for index.js

--- header-parser/server.js
+++ header-parser/server.js
@@ -1,31 +1,31 @@
-"use strict";
+// "use strict";
 
-// let favicon = require('serve-favicon');
-let app = require('express')();
+// // let favicon = require('serve-favicon');
+// let app = require('express')();
 
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+// // app.use(favicon(__dirname + '/public/favicon.ico'));
 
-// app.get("/", (req, res) => {
-//   // return instructions page
-//   res.sendFile(__dirname + "/public/index.html");
+// // app.get("/", (req, res) => {
+// //   // return instructions page
+// //   res.sendFile(__dirname + "/public/index.html");
+// // });
+
+// app.get('/api/whoami', (req, res) => {
+
+//   let info = {
+//     "ipaddress": req.ip,
+//     "language": req.headers["accept-language"].split(",")[0],
+//     "software": req.headers["user-agent"].split("(")[1].split(")")[0]
+//   };
+
+//   console.log(JSON.stringify(info) + "\n");
+
+//   res.set('Content-Type', 'application/json');
+//   res.json(info);
 // });
 
-app.get('/api/whoami', (req, res) => {
+// let port = process.env.PORT || 8080;
 
-  let info = {
-    "ipaddress": req.ip,
-    "language": req.headers["accept-language"].split(",")[0],
-    "software": req.headers["user-agent"].split("(")[1].split(")")[0]
-  };
-
-  console.log(JSON.stringify(info) + "\n");
-
-  res.set('Content-Type', 'application/json');
-  res.json(info);
-});
-
-let port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-  console.log("Server is running on port " + port + "\n");
-});
+// app.listen(port, () => {
+//   console.log("Server is running on port " + port + "\n");
+// });
